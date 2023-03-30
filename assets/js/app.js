d02be67a1dca43f76612e7836237fcd91bf6b4c1
DanieleMorello <danielemorello@live.it>
2023-03-30 18:51:53 +0200

    Add ul and a div (with an input and a button) in html add a google font in css and create an array in js and the functions: toggleTodoDone,removeTodo and addNewTodo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,28 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-    createApp({
-      data() {
-        return {
-          
-        }
-      }
-    }).mount('#app')
+createApp({
+  data() {
+    return {
+      todos: [
+        { text: 'Alzarsi dal letto', done: false },
+        { text: 'Uscire', done: true },
+        { text: 'Studiare per ladocumentazione', done: false }
+      ],
+      newTodoText: '',
+      showWarning: false,
+      errorMessage: "Inserisci almeno 5 caratteri",
+    };
+  },
+  methods: {
+    toggleTodoDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    addNewTodo() {
+      this.todos.push({ text: this.newTodoText, done: false });
+      this.newTodoText = '';
+    }
+  }
+}).mount('#app');
